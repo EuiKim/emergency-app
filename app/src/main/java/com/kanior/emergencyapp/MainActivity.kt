e151bce517bd954ecfb472a634e14142eb1980e6
EuiKim <kim.eui@aibizon.com>
2025-06-26 10:53:19 +0900

    20250626

--- app/src/main/java/com/kanior/emergencyapp/MainActivity.kt
+++ app/src/main/java/com/kanior/emergencyapp/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.kanior.emergencyapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,13 +60,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EmergencyAppTheme {
-                AppNavigation()
+//                AppNavigation()
+//                TextEx()
+//                ButtonEx()
+//                RememberEx()
+//                ColumnEx()
+                TextFieldEx()
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
@@ -130,3 +165,361 @@ fun KorMenu(navController: NavController) {
         }
     }
 }
+
+//@Preview(showBackground = true)
+@Composable
+fun TextEx() {
+    Text(
+        text = "텍스트 예제입니다",
+        fontSize = 30.sp,
+        color = Color.Red,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(30.dp),
+        style = TextStyle(background = Color.Blue)
+    )
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ButtonEx() {
+    val context = LocalContext.current
+
+    Button(
+        onClick = {
+            Toast.makeText(context, "클릭됨", Toast.LENGTH_SHORT).show()
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Black
+        ),
+        modifier = Modifier
+            .width(200.dp)
+            .height(200.dp)
+    ) {
+        Text(
+            text = "버튼 팀버튼 엘버튼 버튼버튼",
+            lineHeight = 40.sp,
+            fontSize = 30.sp,
+            color = Color.Red
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun RememberEx() {
+    var count by remember { mutableIntStateOf(0) }
+
+    Button(
+        onClick = {
+            count++
+        },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Count : $count",
+            fontSize = 30.sp
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ColumnEx() {
+    Column(
+        modifier = Modifier.padding(30.dp)
+    ) {
+        Text(
+            text = "안녕하세요 1",
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.padding(30.dp))
+        HorizontalDivider(
+            thickness = 4.dp,
+            color = Color.Blue
+        )
+        Spacer(modifier = Modifier.padding(30.dp))
+        Text(
+            text = "안녕하세요 2",
+            fontSize = 30.sp
+        )
+        Text(
+            text = "안녕하세요 3",
+            fontSize = 30.sp
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun TextFieldEx() {
+    var textState by remember { mutableStateOf("") }
+    var enteredText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = textState,
+            onValueChange = {
+                textState = it
+            },
+            label = {
+                Text("입력하는 공간")
+            }
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        OutlinedTextField(
+            value = textState,
+            onValueChange = {
+                textState = it
+            },
+            label = {
+                Text("입력하는 공간")
+            }
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier.height(60.dp)
+        ) {
+            TextField(
+                value = textState,
+                onValueChange = {
+                    textState = it
+                },
+                label = {
+                    Text("입력하는 공간")
+                },
+                modifier = Modifier
+                    .width(210.dp)
+                    .padding(end = 10.dp)
+            )
+            Button(
+                onClick = {
+                    enteredText = textState
+                },
+                modifier = Modifier
+                    .width(80.dp)
+                    .height(50.dp)
+            ) {
+                Text("입력")
+            }
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text(text = "결과값 : $enteredText")
+        Spacer(modifier = Modifier.padding(10.dp))
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ImageEx() {
+    Image(
+        painter = painterResource(id = R.drawable.sample),
+        contentDescription = "sample"
+    )
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun BoxEx() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Text("왼쪽 위")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Text("중앙 위")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Text("오른쪽 위")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Text("중앙 왼쪽")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text("정중앙")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Text("중앙 오른쪽")
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun RowEx() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray),
+        horizontalArrangement = Arrangement.SpaceEvenly, // 가로로 동일한 간격
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Item1",
+            style = TextStyle(background = Color.Blue),
+            fontSize = 30.sp
+        )
+        Text(
+            text = "Item2",
+            style = TextStyle(background = Color.Red),
+            fontSize = 30.sp
+        )
+        Text(
+            text = "Item3",
+            style = TextStyle(background = Color.Green),
+            fontSize = 30.sp
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ColumnRowEx() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(Color.Gray)
+    ) {
+        Text(
+            text = "안녕하세요",
+            color = Color.Blue,
+            fontSize = 20.sp
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+//            horizontalArrangement = Arrangement.SpaceAround
+//            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text("왼쪽")
+            Text("중앙")
+            Text("오른쪽")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColumnRowEx2() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .background(Color.Cyan)
+            .border(BorderStroke(5.dp, Color.Blue)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier.padding(top = 20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sample),
+                contentDescription = "profile image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(50.dp))
+            )
+        }
+        Text(
+            text = "개발자 A",
+            color = Color.Blue,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 50.dp)
+        )
+        Text(
+            text = "Developer",
+            fontSize = 15.sp,
+            modifier = Modifier.padding(10.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "이메일",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "kanior@naver.com",
+                fontSize = 15.sp,
+                modifier = Modifier.padding(10.dp),
+                color = Color.Blue
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "연락처",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "010-1111-2222",
+                fontSize = 15.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
